0d34a20212d767bd7870ba55e2714dcef0427fcf
boldak <boldak.andrey@gmail.com>
2023-01-13 20:16:15 +0200

    initial commit)

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongo = require('mongodb').MongoClient 
+
+const getEnv = () => {
+  const keys = ["APP_HOST", "APP_PROTOCOL","PORT","ATLAS_URL","MONGO_URI","GOOGLE_CLIENT_ID","GOOGLE_CLIENT_SECRET","GOOGLE_CALLBACK",]
+  let res = {}
+  keys.forEach( key => {
+    res[key] = process.env[key]
+  })
+  return res
+}
+
+console.log(getEnv())
+
+const app = express();
+
+app.use('/', async(req, res) => {
+	let client = await mongo.connect(process.env.ATLAS_URL, {
+	    useNewUrlParser: true,
+	    useUnifiedTopology: true
+	 })
+	
+	let result = await	client
+						.db("sparrow-dev")
+						.collection("app-grant")
+						.aggregate([{$match:{}}])
+						.toArray()
+	res.send(result)					
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Service starts on port ${process.env.PORT}`);
+});
+
+
+
